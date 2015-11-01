8ab615fbf3ab525e15ea771dfcb9ed6cabd14267
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-01 18:25:56 +0900

    コメント部分のフォーマットを修正

--- src/main/Lists.hs
+++ src/main/Lists.hs
@@ -30,11 +30,11 @@ module Lists where
   myButLast [x, _] = x
   myButLast (_:xs) = myButLast xs
 
-  {-3 Problem 3
+  {-
+  - 3 Problem 3
   - (*) Find the K'th element of a list. The first element in the list is number 1.
   -
   - Example in Haskell:
-  -
   - Prelude> elementAt [1,2,3] 2
   - 2
   - Prelude> elementAt "haskell" 5
