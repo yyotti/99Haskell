ad0045cfb95c283c9659ced4e4db5869cd7434ad
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-02 10:29:41 +0900

    テストケースの順番を入れ替え

--- src/test/ListsSpec.hs
+++ src/test/ListsSpec.hs
@@ -93,17 +93,17 @@ spec = do
       compress "aaaabccaadeeee" `shouldBe` "abcade"
 
     it "returns compressed list (2)" $ do
-      compress ([] :: [Char]) `shouldBe` []
-
-    it "returns compressed list (3)" $ do
       compress [1 :: Int, 2, 3, 4] `shouldBe` [1, 2, 3, 4]
+
+    it "returns empty list" $ do
+      compress ([] :: [Char]) `shouldBe` []
 
   describe "Problem 9" $ do
     it "returns packed list (1)" $ do
       pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'] `shouldBe` ["aaaa", "b", "cc", "aa", "d", "eeee"]
 
     it "returns packed list (2)" $ do
-      pack ([] :: [Int]) `shouldBe` []
-
-    it "returns packed list (3)" $ do
       pack [1 :: Int, 2, 3, 4] `shouldBe` [[1], [2], [3], [4]]
+
+    it "returns empty list" $ do
+      pack ([] :: [Int]) `shouldBe` []
