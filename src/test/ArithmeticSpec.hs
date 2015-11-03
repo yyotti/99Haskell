8e6f2506c90a9141d05c7bcd4a10ece1f5f98725
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-03 09:54:59 +0900

    テストの記述パターンを変更

--- src/test/ArithmeticSpec.hs
+++ src/test/ArithmeticSpec.hs
@@ -9,34 +9,34 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Problem 31" $ do
-    it "returns true (1)" $ do
+    it "returns true when n = 7" $ do
       isPrime 7 `shouldBe` True
 
-    it "returns true (2)" $ do
+    it "returns true when n = 2" $ do
       isPrime 2 `shouldBe` True
 
-    it "returns false (1)" $ do
+    it "returns false when n = 6" $ do
       isPrime 6 `shouldBe` False
 
-    it "returns false (2)" $ do
+    it "returns false when n = 1" $ do
       isPrime 1 `shouldBe` False
 
   describe "Problem 32" $ do
-    it "returns GCD (1)" $ do
+    it "returns 9 when (m, n) = (36, 63)" $ do
       myGCD 36 63 `shouldBe` 9
 
-    it "returns GCD (2)" $ do
+    it "returns 3 when (m, n) = (-3, -6)" $ do
       myGCD (-3) (-6) `shouldBe` 3
 
-    it "returns GCD (3)" $ do
+    it "returns 3 when (m, n) = (-3, 6)" $ do
       myGCD (-3) 6 `shouldBe` 3
 
   describe "Problem 33" $ do
-    it "returns True (1)" $ do
+    it "returns True when (m, n) = (35, 64)" $ do
       coprime 35 64 `shouldBe` True
 
-    it "returns True (2)" $ do
+    it "returns True when (m, n) = (109, 2)" $ do
       coprime 109 2 `shouldBe` True
 
-    it "returns False (1)" $ do
+    it "returns False when (m, n) = (4, 14)" $ do
       coprime 4 14 `shouldBe` False
